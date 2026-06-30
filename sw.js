8ff7889f98de4bff4b2848ce3da611f7389bcc42
Panda Man 🐼 <219752327+PandaManOfficial420@users.noreply.github.com>
2026-06-30 17:01:36 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = "desktop-files-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response =>
+        response || fetch(event.request)
+      )
+  );
+});
